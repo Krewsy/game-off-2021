27a7f43bdd4d3e6593ceac1f3e9b807145a6aabf
KruSuPhy <krusuphy@hotmail.com>
2021-11-05 21:03:29 -0400

    implement failure state and try again button

--- Assets/Scripts/TryAgain.cs
+++ Assets/Scripts/TryAgain.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TryAgain : MonoBehaviour
 {
+    public Button yourButton; 
+
     // Start is called before the first frame update
     void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
     {
         SceneManager.LoadScene("GameScene");
     }
